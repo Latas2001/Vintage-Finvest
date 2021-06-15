b3df9a6b5cb8331e7f41cd370373ec2c673c0a47
Latas2001 <latasharmaqq@gmail.com>
2021-06-15 20:14:39 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        lightblue: 'colors.lightblue',
+        blue: '#09244a',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
