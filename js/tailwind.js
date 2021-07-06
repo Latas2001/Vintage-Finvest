de6bcc549747751380cd549fb496d2327a41faa8
Latas2001 <latasharmaqq@gmail.com>
2021-07-06 19:57:17 +0530

    modified services section in index.html and attached js files to tailwind.js

--- js/tailwind.js
+++ js/tailwind.js
@@ -1,3 +1,4 @@
+/*Carousal Section*/ 
 
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
@@ -15,3 +16,114 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+  $(".mySwiper").hover(function() {
+    (this).swiper.autoplay.stop();
+    }, 
+    function() {
+    (this).swiper.autoplay.start();
+  });
+
+/*Counter section*/
+
+var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('.count').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    counted = 1;
+  }
+
+});
+
+/*Testimonials*/
+
+var cont=0;
+function loopSlider(){
+  var xx= setInterval(function(){
+        switch(cont)
+        {
+        case 0:{
+            $("#slider-1").fadeOut(400);
+            $("#slider-2").delay(400).fadeIn(400);
+            $("#sButton1").removeClass("bg-blue-800");
+            $("#sButton2").addClass("bg-blue-800");
+        cont=1;
+        
+        break;
+        }
+        case 1:
+        {
+        
+            $("#slider-2").fadeOut(400);
+            $("#slider-1").delay(400).fadeIn(400);
+            $("#sButton2").removeClass("bg-blue-800");
+            $("#sButton1").addClass("bg-blue-800");
+           
+        cont=0;
+        
+        break;
+        }
+        
+        
+        }},8000);
+
+}
+
+function reinitLoop(time){
+clearInterval(xx);
+setTimeout(loopSlider(),time);
+}
+
+
+
+function sliderButton1(){
+
+    $("#slider-2").fadeOut(400);
+    $("#slider-1").delay(400).fadeIn(400);
+    $("#sButton2").removeClass("bg-blue-800");
+    $("#sButton1").addClass("bg-blue-800");
+    reinitLoop(4000);
+    cont=0
+    
+    }
+    
+    function sliderButton2(){
+    $("#slider-1").fadeOut(400);
+    $("#slider-2").delay(400).fadeIn(400);
+    $("#sButton1").removeClass("bg-blue-800");
+    $("#sButton2").addClass("bg-blue-800");
+    reinitLoop(4000);
+    cont=1
+    
+    }
+
+    $(window).ready(function(){
+        $("#slider-2").hide();
+        $("#sButton1").addClass("bg-blue-800");
+        
+
+        loopSlider();
+     });
